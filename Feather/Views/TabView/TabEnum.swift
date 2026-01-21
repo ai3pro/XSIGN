d3ca3a7f6edb7ec055a527aad7987be88067a22c
ai3pro <tranngoczxc12321@gmail.com>
2026-01-21 21:21:52 +0700

    Update TabEnum.swift

--- Feather/Views/TabView/TabEnum.swift
+++ Feather/Views/TabView/TabEnum.swift
@@ -1,58 +1,23 @@
-//
-//  TabEnum.swift
-//  feather
-//
-//  Created by samara on 22.03.2025.
-//
-
 import SwiftUI
-import NimbleViews
 
-enum TabEnum: String, CaseIterable, Hashable {
-	case sources
-	case library
-	case settings
-	case certificates
-	
-	var title: String {
-		switch self {
-		case .sources:     	return .localized("Sources")
-		case .library: 		return .localized("Library")
-		case .settings: 	return .localized("Settings")
-		case .certificates:	return .localized("Certificates")
-		}
-	}
-	
-	var icon: String {
-		switch self {
-		case .sources: 		return "globe.desk"
-		case .library: 		return "square.grid.2x2"
-		case .settings: 	return "gearshape.2"
-		case .certificates: return "person.text.rectangle"
-		}
-	}
-	
-	@ViewBuilder
-	static func view(for tab: TabEnum) -> some View {
-		switch tab {
-		case .sources: SourcesView()
-		case .library: LibraryView()
-		case .settings: SettingsView()
-		case .certificates: NBNavigationView(.localized("Certificates")) { CertificatesView() }
-		}
-	}
-	
-	static var defaultTabs: [TabEnum] {
-		return [
-			.sources,
-			.library,
-			.settings
-		]
-	}
-	
-	static var customizableTabs: [TabEnum] {
-		return [
-			.certificates
-		]
-	}
+enum Tab: String, CaseIterable {
+    case library
+    case buyCert // Tab VIP mới
+    case settings
+    
+    var title: LocalizedStringKey {
+        switch self {
+        case .library: return "Kho Ứng Dụng"
+        case .buyCert: return "Mua VIP"
+        case .settings: return "Cài Đặt"
+        }
+    }
+    
+    var icon: String {
+        switch self {
+        case .library: return "square.stack.3d.up.fill"
+        case .buyCert: return "crown.fill" // Icon Vương miện cho VIP
+        case .settings: return "gearshape.fill"
+        }
+    }
 }
