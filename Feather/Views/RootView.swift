0efbd1590257b28f3c335f4b28ef35dc3299813a
ai3pro <tranngoczxc12321@gmail.com>
2026-01-29 14:47:22 +0700

    Update RootView.swift

--- Feather/Views/RootView.swift
+++ Feather/Views/RootView.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
 struct RootView: View {
-    @State var currentTab: AppTab = .library // Dùng AppTab
+    @State var currentTab: AppTab = .library
     
     init() {
         UITabBar.appearance().isHidden = true
@@ -9,12 +9,21 @@ struct RootView: View {
     
     var body: some View {
         ZStack(alignment: .bottom) {
+            
+            // 1. NỘI DUNG CHÍNH
             TabView(selection: $currentTab) {
                 NavigationView {
                     LibraryView()
                         .navigationTitle("Kho Ứng Dụng")
                 }
                 .tag(AppTab.library)
+                
+                // Thêm màn hình Nguồn
+                NavigationView {
+                    SourcesView()
+                        .navigationTitle("Nguồn iPA")
+                }
+                .tag(AppTab.sources)
                 
                 NavigationView {
                     BuyCertView()
@@ -26,7 +35,12 @@ struct RootView: View {
                 }
                 .tag(AppTab.settings)
             }
+            // FIX LỖI THỤT: Đẩy nội dung lên để không bị Tabbar che mất
+            .safeAreaInset(edge: .bottom) {
+                Color.clear.frame(height: 80)
+            }
             
+            // 2. THANH TAB NỔI
             CustomTabBar(currentTab: $currentTab)
                 .padding(.bottom, 10)
         }
